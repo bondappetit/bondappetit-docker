d15631294feff8b6ad0235801acb02ee784d9bd6
Vladislav Komissarov <mastyf@gmail.com>
2021-04-02 15:59:19 +0300

    ganache-ropsten container

--- config/services/ganache-ropsten.run.sh
+++ config/services/ganache-ropsten.run.sh
@@ -2,4 +2,4 @@
 
 npm init
 npm i --save ganache-cli
-npx ganache-cli -f https://ropsten.bondappetit.io -m 'tape total toast night perfect myself kiwi rich glow moment bitter actual spare bottom boss' -u 0x8d22dbDD383Eff153025108f803AB3F2CFf6c795 -i 1 --chainId 1
+npx ganache-cli -f https://ropsten.bondappetit.io -m 'tape total toast night perfect myself kiwi rich glow moment bitter actual spare bottom boss' -u 0x8d22dbDD383Eff153025108f803AB3F2CFf6c795 -i 3 --chainId 3
