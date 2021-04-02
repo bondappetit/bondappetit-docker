76a9cd881f9c356ec89641c5518c20a477417125
Vladislav Komissarov <mastyf@gmail.com>
2021-04-02 17:10:59 +0300

    fix for chainID

--- config/services/ganache-mainnet.run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-ganache-cli -f http://ba-mainnet:8545 -m 'tape total toast night perfect myself kiwi rich glow moment bitter actual spare bottom boss' -h 0.0.0.0 -u 0x8d22dbDD383Eff153025108f803AB3F2CFf6c795 -i 1 --chainId 1
