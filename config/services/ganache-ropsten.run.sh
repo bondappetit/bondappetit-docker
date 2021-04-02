9c91a61d3cbc414bd506c315c24b5d33e125078f
Vladislav Komissarov <mastyf@gmail.com>
2021-04-02 16:17:40 +0300

    fix for ganache

--- config/services/ganache-ropsten.run.sh
+++ config/services/ganache-ropsten.run.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-npm init
-npm i --save ganache-cli
-npx ganache-cli -f http://ba-ropsten:8545 -m 'tape total toast night perfect myself kiwi rich glow moment bitter actual spare bottom boss' -h 0.0.0.0 -u 0x8d22dbDD383Eff153025108f803AB3F2CFf6c795 -i 3 --chainId 3
+ganache-cli -f http://ba-ropsten:8545 -m 'tape total toast night perfect myself kiwi rich glow moment bitter actual spare bottom boss' -h 0.0.0.0 -u 0x8d22dbDD383Eff153025108f803AB3F2CFf6c795 -i 3 --chainId 3
